832a4c7fe6c1be3d4003ba7b41a443625b38313f
Ashish_Khati <khatiashish2442@gmail.com>
2021-07-11 20:55:09 +0530

    Initial commit

---
+++ src/com/ArraysStringsAndVectors/Array.java
@@ -0,0 +1,108 @@
+/*
+ * -------------RULES YOU SHOULD KNOW--------------------
+ * @Array is a group of contiguous or related data items that share a common name.
+ * @
+ */
+package com.ArraysStringsAndVectors;
+import java.util.Scanner;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*	OneDimonsionArray oda=new OneDimonsionArray();
+		oda.getName();
+        oda.getSalary();
+		oda.showDetails();
+    */
+		
+		//Sort class (Example 2) object creation
+		Sort s=new Sort();
+		s.sorting();
+
+	}
+
+}
+
+
+//Single Dimonsion Array Example
+
+
+class OneDimonsionArray
+{
+	static int count=1;
+	static int countSalary=1;
+ String name[]=new String[5];
+ int salary[]=new int[5];
+	Scanner sc=new Scanner(System.in);
+	
+	public void getName()
+	{
+		 
+		for(int i=0;i<name.length;i++)
+		{
+			System.out.print("Enter name of employee"+count+": ");
+			name[i]=sc.nextLine();
+			count++;
+		}
+	
+	}
+	public void getSalary()
+	{
+		for(int i=0;i<salary.length;i++)
+		{
+			System.out.print("\n\nEnter salary of employee"+countSalary+": ");
+			salary[i]=sc.nextInt();
+			countSalary++;
+		}
+	}
+	public void showDetails()
+	{
+		System.out.printf("\n--------------------------------------------------------------\n");
+		System.out.printf("			Employee Details			");
+		
+		for(int i=0;i<5;i++)
+		{	
+			
+			System.out.printf("\n%-8s%10d\n",name[i],salary[i]);
+		}
+	}
+}
+
+
+
+//One dimensional array example 2
+class Sort
+{
+	int number[]=new int[] {12,78,45,98,11,57,87};
+	int temp;
+	public void sorting()
+	{
+		System.out.print("Numbers before sorting: ");
+		for(int i:number)
+		{
+			System.out.print("\t"+i);
+		}
+		
+		for(int i=0;i<number.length;i++)
+		{
+			for(int j=i+1;j<number.length;j++)
+			{
+				if(number[i]>number[j])
+				{
+					temp=number[i];
+					number[i]=number[j];
+					number[j]=temp;
+				}
+			}
+		}
+		System.out.print("\nNumbers after sorting: ");
+	for(int i:number)
+		
+	{
+		System.out.print(" \t"+i);
+	}
+	}
+	
+	
+}
